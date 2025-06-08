4a2546cb07d1cdabd17991fa83fd08160a275940
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-06-08 22:59:05 +0200

    instalation libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+main.py: třetí projekt do Engeto Online Python Akademie
+
+author: Jan Kuděla
+email: jochanan.jorgen@gmail.com
+"""
+
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+
