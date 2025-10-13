db51ce66b45f038cad73fa4f5740b42c2e2c3cf2
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-10-13 19:19:35 +0200

    vysoupovány - název obce, počet voličů, obálky, platné hlasy, strany, hlasy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import requests
 from requests import get
 from bs4 import BeautifulSoup
 
-url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7101"
+url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
 odpoved = requests.get(url)
 if odpoved.status_code != 200:
     print("Chyba při načítání stránky:", odpoved.status_code)
@@ -42,6 +42,31 @@ if odpoved_2.status_code != 200:
 
 parsovane_html_2 = BeautifulSoup(odpoved_2.text, features="html.parser")
 nazev_obce = parsovane_html_2.find_all("h3")[2].get_text()
-#print(nazev_obce)
 
-volici_v_seznamu = parsovane_html_2.find_all("tbody")
+print(nazev_obce[6:])
+
+volici_v_seznamu = parsovane_html_2.find_all("td")[3].get_text()
+print(volici_v_seznamu)
+
+vydane_obalky = parsovane_html_2.find_all("td")[4].get_text()
+print(vydane_obalky)
+
+platne_hlasy = parsovane_html_2.find_all("td")[7].get_text()
+print(platne_hlasy)
+
+strana = parsovane_html_2.find_all("td", {"class": "overflow_name"})
+#print(strana)
+
+strana_soupis = []
+for a in strana:
+    text = a.get_text(strip=True)
+    strana_soupis.append(text)
+
+print(strana_soupis)
+
+strana_hlasy = parsovane_html_2.find_all("td", {"class": "cislo", "headers": "t1sa2 t1sb3"})
+hlasy_soupis = []
+for a in strana_hlasy:
+    cislo = a.get_text(strip=True)
+    hlasy_soupis.append(cislo)
+print(hlasy_soupis) 
